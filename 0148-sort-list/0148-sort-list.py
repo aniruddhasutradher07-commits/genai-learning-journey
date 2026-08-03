37ea22ce2158143424945ff20208a0b3aefbe3cb
Aniruddha <aniruddhasutradher07@gmail.com>
2026-08-03 12:26:39 +0530

    Time: 436 ms (37.99%), Space: 50.6 MB (75.66%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.py
@@ -0,0 +1,40 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def sortList(self, head):
+        if not head or not head.next:
+            return head
+
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second_half = slow.next
+        slow.next = None
+
+        left = self.sortList(head)
+        right = self.sortList(second_half)
+
+        return self.merge(left, right)
+
+    def merge(self, l1, l2):
+        dummy = ListNode(-1)
+        current = dummy
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+            current = current.next
+
+        current.next = l1 if l1 else l2
+
+        return dummy.next
