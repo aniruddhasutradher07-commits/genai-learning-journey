951176b470abb064cc5048c7df95db8fb9dc7d68
Aniruddha <aniruddhasutradher07@gmail.com>
2026-09-12 23:52:19 +0530

    Time: 1 ms (39.15%), Space: 19.2 MB (87.99%) - LeetHub

---
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.py
@@ -0,0 +1,13 @@
+class Solution:
+    def transformArray(self, nums: List[int]) -> List[int]:
+        ans = []
+
+        for num in nums:
+            if num % 2 == 0:
+                ans.append(0)
+            else:
+                ans.append(1)
+
+        ans.sort()
+
+        return ans           
