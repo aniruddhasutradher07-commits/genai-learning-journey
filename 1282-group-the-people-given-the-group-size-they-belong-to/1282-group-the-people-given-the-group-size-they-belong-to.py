502ea86d9cc8bf25e9d4846764db233c6c32a6ec
Aniruddha <aniruddhasutradher07@gmail.com>
2026-09-16 14:45:17 +0530

    Time: 3 ms (68.2%), Space: 19.3 MB (86.8%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        group_map = defaultdict(list)
+        result = []
+
+        for person_id, size in enumerate(groupSizes):
+            group_map[size].append(person_id)
+
+            if len(group_map[size]) == size:
+                result.append(group_map[size])
+                group_map[size] = []
+
+        return result        
+        
